be8fc83419f9afed4f1997eb18c5439cf6b818bd
Avery2 <JustinAveryChan@gmail.com>
2020-01-14 14:52:48 -0600

    fully changed to forms addon from sheet addon

--- Code.js
+++ Code.js
@@ -1,68 +1,80 @@
 /**
-* Creates a trigger for when form entered.
-*/
+ * Creates a trigger for when form entered.
+ */
 function createSpreadsheetOpenTrigger() {
+  // test message
   var form = FormApp.getActiveForm();
   try {
-    ScriptApp.newTrigger('exportRecentAsDoc').forForm(form).onFormSubmit().create();
+    deleteAllTriggers();
+    ScriptApp.newTrigger("exportRecentAsDoc")
+      .forForm(form)
+      .onFormSubmit()
+      .create();
   } catch (error) {
-    SpreadsheetApp.getUi().alert('Confirmation received.');
+    SpreadsheetApp.getUi().alert("Confirmation received.");
   }
 }
 
 /**
-* Exports most recent entry to spreadsheet from form as a Google Doc.
-*/
+ * Exports most recent entry to spreadsheet from form as a Google Doc.
+ */
 function exportRecentAsDoc() {
-  // setup
+  // SETUP
   var form = FormApp.getActiveForm();
   var d = new Date(); // create date object (for testing purposes)
-  
-  // parse, to get last row's data
-  var items = form.getItems();
-  var responses = form.getResponses();
-  var titles = items.forEach(function(item, index){
-                             titles[index] = item.getTitle();
-                             });
-  var latestResponse = responses[responses.length-1];
-  
+
+  // READ FORM ENTRIES
+  // parse, to get last row's data and all questions
+  var titles = [];
+  form.getItems().forEach(function(item, index) {
+    titles[index] = item.getTitle();
+  });
+  // TODO error handling if empty (will be empty array)
+  Logger.log(form.getResponses());
+  var latestResponses = form.getResponses()[form.getResponses().length - 1].getItemResponses();
+
+  // MAKE DOCUMENT
   // create doc
   var doc = DocumentApp.create(d.toLocaleTimeString());
-  
   // save to folder
-  var folder = DriveApp.getFoldersByName("Contact Information (Documents)").next();
+  // TODO automate this, and create folder if not found
+  var folder = DriveApp.getFoldersByName(
+    "Contact Information (Documents)"
+  ).next();
   folder.addFile(DriveApp.getFileById(doc.getId())); // creates second reference to same file
   // remove from root (default) folder, which was automatically placed there on creation
   DriveApp.getRootFolder().removeFile(DriveApp.getFileById(doc.getId()));
-  
-  // fill doc with parsed data
+
+  // FILL DOCUMENT
   var body = doc.getBody();
-  // body.appendParagraph("Name: "+name+" Contact Info: "+info);
-  
   // format style
   headerStyle = {};
-//  headerStyle[DocumentApp.Attribute.FONT_SIZE] = 18;
-//  headerStyle[DocumentApp.Attribute.BOLD] = true;
-  headerStyle[DocumentApp.Attribute.HEADING] = DocumentApp.ParagraphHeading.HEADING1;
+  //  headerStyle[DocumentApp.Attribute.FONT_SIZE] = 18;
+  //  headerStyle[DocumentApp.Attribute.BOLD] = true;
+  headerStyle[DocumentApp.Attribute.HEADING] =
+    DocumentApp.ParagraphHeading.HEADING1;
   bodyStyle = {};
-  
+
   Logger.clear();
-  headers.forEach(function(currentValue) {Logger.log(currentValue)});
-  lastRow.forEach(function(currentValue) {Logger.log(currentValue)});
-  
-  // FILL DOC HERE
-  lastRow.forEach(function(currentValue, index) {
-    //    body.appendParagraph(''+headers[index]).setAttributes(headerStyle);
-    body.appendParagraph(''+headers[index]).setAttributes(headerStyle);
-    body.appendParagraph(''+lastRow[index]).setAttributes(bodyStyle);
+  titles.forEach(function(currentValue) {
+    Logger.log(currentValue);
+  });
+  latestResponses.forEach(function(currentValue) {
+    Logger.log(currentValue);
+  });
+
+  // TODO fill doc here
+  latestResponses.forEach(function(currentValue, index) {
+    body.appendParagraph("" + titles[index]).setAttributes(headerStyle);
+    body.appendParagraph("" + latestResponses[index]).setAttributes(bodyStyle);
     body.appendHorizontalRule();
   });
 }
 
 /**
-* Deletes all triggers.
-*/
-function deleteAllTrigger() {
+ * Deletes all triggers.
+ */
+function deleteAllTriggers() {
   // Loop over all triggers.
   var allTriggers = ScriptApp.getProjectTriggers();
   for (var i = 0; i < allTriggers.length; i++) {
