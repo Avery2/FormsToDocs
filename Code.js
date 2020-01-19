48b9bc0e601b30a06d691090b57a33d8fc6670ca
Avery2 <JustinAveryChan@gmail.com>
2020-01-18 21:04:00 -0600

    clean up addon, remove leftover functions from template

--- Code.js
+++ Code.js
@@ -28,6 +28,8 @@ function onOpen(e) {
  */
 function onInstall(e) {
   onOpen(e);
+  // deleteAllTriggers();
+  createSpreadsheetOpenTrigger();
 }
 
 /**
@@ -61,7 +63,6 @@ function showAbout() {
  */
 function saveSettings(settings) {
   PropertiesService.getDocumentProperties().setProperties(settings);
-  // adjustFormSubmitTrigger();
 }
 
 /**
@@ -98,73 +99,26 @@ function getSettings() {
   return settings;
 }
 
-/**
- * Adjust the onFormSubmit trigger based on user's requests.
- */
-// function adjustFormSubmitTrigger() {
-//   var form = FormApp.getActiveForm();
-//   var triggers = ScriptApp.getUserTriggers(form);
-//   var settings = PropertiesService.getDocumentProperties();
-//   var triggerNeeded =
-//     settings.getProperty("creatorNotify") == "true" ||
-//     settings.getProperty("respondentNotify") == "true";
-
-//   // Create a new trigger if required; delete existing trigger
-//   //   if it is not needed.
-//   var existingTrigger = null;
-//   for (var i = 0; i < triggers.length; i++) {
-//     if (triggers[i].getEventType() == ScriptApp.EventType.ON_FORM_SUBMIT) {
-//       existingTrigger = triggers[i];
-//       break;
-//     }
-//   }
-//   if (triggerNeeded && !existingTrigger) {
-//     var trigger = ScriptApp.newTrigger("respondToFormSubmit")
-//       .forForm(form)
-//       .onFormSubmit()
-//       .create();
-//   } else if (!triggerNeeded && existingTrigger) {
-//     ScriptApp.deleteTrigger(existingTrigger);
-//   }
-// }
+// usage, settings.getProperty("SettingA") == "true";
 
 /**
  * Responds to a form submission event if an onFormSubmit trigger has been
- * enabled.
+ * enabled. BROKE SAVED FOR REFERENCE
  *
  * @param {Object} e The event parameter created by a form
  *      submission; see
  *      https://developers.google.com/apps-script/understanding_events
  */
-// function respondToFormSubmit(e) {
 //   var settings = PropertiesService.getDocumentProperties();
 //   var authInfo = ScriptApp.getAuthorizationInfo(ScriptApp.AuthMode.FULL);
-
-//   // Check if the actions of the trigger require authorizations that have not
-//   // been supplied yet -- if so, warn the active user via email (if possible).
-//   // This check is required when using triggers with add-ons to maintain
-//   // functional triggers.
 //   if (
 //     authInfo.getAuthorizationStatus() == ScriptApp.AuthorizationStatus.REQUIRED
 //   ) {
-//     // Re-authorization is required. In this case, the user needs to be alerted
-//     // that they need to reauthorize; the normal trigger action is not
-//     // conducted, since authorization needs to be provided first. Send at
-//     // most one 'Authorization Required' email a day, to avoid spamming users
-//     // of the add-on.
 //     sendReauthorizationRequest();
 //   } else {
-//     // All required authorizations have been granted, so continue to respond to
-//     // the trigger event.
-
-//     // Check if the form creator needs to be notified; if so, construct and
-//     // send the notification.
 //     if (settings.getProperty("creatorNotify") == "true") {
 //       sendCreatorNotification();
 //     }
-
-//     // Check if the form respondent needs to be notified; if so, construct and
-//     // send the notification. Be sure to respect the remaining email quota.
 //     if (
 //       settings.getProperty("respondentNotify") == "true" &&
 //       MailApp.getRemainingDailyQuota() > 0
